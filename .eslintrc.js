ba6d43856580eca51357575b6e2c3560a6e4b5dd
Nhan Nguyen <nhannguyenmath95@gmail.com>
2021-01-17 22:18:28 +0900

    ALC-27: adopt typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,22 @@ module.exports = {
         "es2021": true,
         "jest/globals": true
     },
+    "parser": "@typescript-eslint/parser", // Specifies the ESLint parser
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
     ],
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 12,
+        "ecmaVersion": 2020,
         "sourceType": "module"
+    },
+    "settings": {
+        "react": {
+            "version": "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+        }
     },
     "plugins": [
         "react",
@@ -23,6 +29,6 @@ module.exports = {
         // suppress errors for missing 'import React' in files
         "react/react-in-jsx-scope": "off",
         // allow jsx syntax in js files (for next.js project)
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts"] }], //should add ".ts" if typescript project
     }
 };
